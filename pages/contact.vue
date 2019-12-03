733febfc8c5de37e633a36599c5a0eeed526bb88
masayukinii1011 <masayukinii1011@gmail.com>
2019-12-03 22:50:25 +0900

    フォーム記述変更

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,24 +1,19 @@
 <template>
   <div>
     <page-title-area v-bind:title="$nuxt.$route.name" />
-<form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>
+<form name="contact" netlify-honeypot="bot-field" action="thank-you"  data-netlify="true" method="post">
+  <input type="hidden" name="form-name" value="contact" />
+  <p class="hidden"  style="display: none;">
+    <label>Don’t fill this out: <input name="bot-field"></label>
   </p>
   <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
+    <label>Email: <input type="text" name="name"></label>
   </p>
   <p>
     <label>Message: <textarea name="message"></textarea></label>
   </p>
   <p>
-    <button type="submit">Send</button>
+    <button type=”submit”>Send</button>
   </p>
 </form>
   </div>
