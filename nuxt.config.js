0a6f86f370365b91f5571484a9bae9e1d5c0bdfa
masayukinii1011 <masayukinii1011@gmail.com>
2019-12-10 01:29:33 +0900

    lang属性、title,description設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,11 +8,14 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Masayuki Nii Portfolio',
+    htmlAttrs: {
+      lang: 'ja'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'Masayuki Nii Portfolio' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
