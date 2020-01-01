95c09f25e8ffe7485e46dff05c215b47f30cbfd2
masayukinii1011 <masayukinii1011@gmail.com>
2020-01-02 01:48:52 +0900

    フォームバリデーション

--- pages/contact.vue
+++ pages/contact.vue
@@ -4,29 +4,29 @@
       <PageTitle :title="$nuxt.$route.name" />
       <div class="columns is-centered">
         <div class="column is-half">
-          <form name="contact" method="POST" data-netlify="true">
+          <form name="contact" method="post" data-netlify="true">
             <input type="hidden" name="form-name" value="contact" />
             <div class="field">
               <label class="label is-size-5">Name</label>
               <div class="control">
-                <input class="input is-medium" name="name" type="text" />
+                <input v-model="name" class="input is-medium" name="name" type="text" />
               </div>
             </div>
             <div class="field">
               <label class="label is-size-5">Email</label>
               <div class="control">
-                <input class="input is-medium" name="email" type="email" />
+                <input v-model="email" class="input is-medium" name="email" type="email" />
               </div>
             </div>
             <div class="field">
               <label class="label is-size-5">Message</label>
               <div class="control">
-                <textarea class="textarea is-large" name="message"></textarea>
+                <textarea v-model="message" class="textarea is-large" name="message"></textarea>
               </div>
             </div>
             <div class="control">
               <button
-                :disabled="true"
+                :disabled="checkForm"
                 type="submit"
                 class="button is-success is-fullwidth is-large"
               >Send Message</button>
@@ -44,6 +44,28 @@ import PageTitle from "~/components/PageTitle.vue";
 export default {
   components: {
     PageTitle
+  },
+  data: function() {
+    return {
+      name: null,
+      email: null,
+      message: null
+    };
+  },
+  computed: {
+    checkForm: function() {
+      if (this.name && this.validEmail(this.email) && this.message) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  },
+  methods: {
+    validEmail: function(email) {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    }
   }
 };
 </script>
