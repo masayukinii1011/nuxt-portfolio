1657c81ac98c921a5ec47ab6584dfde497ce5c3d
masayukinii1011 <masayukinii1011@gmail.com>
2020-01-01 23:04:08 +0900

    タイトル変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,14 +8,14 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: 'Masayuki Nii Portfolio',
+    title: 'msykn portfolio',
     htmlAttrs: {
       lang: 'ja'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Masayuki Nii Portfolio' }
+      { hid: 'description', name: 'description', content: 'msykn portfolio' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
