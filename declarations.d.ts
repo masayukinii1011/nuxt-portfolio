ad4eef3df6e9f494a24fe072081f4772c366d144
masayukinii1011 <masayukinii1011@gmail.com>
2020-09-21 23:38:53 +0900

    環境変数に型追加

---
+++ declarations.d.ts
@@ -0,0 +1,7 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly CTF_SPACE_ID: string;
+    readonly CTF_BLOG_POST_TYPE_ID: string;
+    readonly CTF_CDA_ACCESS_TOKEN: string;
+  }
+}
