24d4ddfab34221d4211e7d9f7fe8ac6ec0aabf71
masayukinii1011 <masayukinii1011@gmail.com>
2020-09-21 23:39:45 +0900

    middlewareをtsに

--- middleware/getContentful.js
+++
@@ -1,4 +0,0 @@
-export default async ({ store }) => {
-  if (!store.state.about || !store.state.works.length) await store.dispatch('getPosts')
-  if (!store.state.categories.length) await store.dispatch('getCategories')
-}
---
+++ middleware/getContentful.ts
@@ -0,0 +1,9 @@
+import { Middleware } from "@nuxt/types";
+
+const getContentful: Middleware = async ({ store }) => {
+  if (!store.state.about || !store.state.works.length)
+    await store.dispatch("getPosts");
+  if (!store.state.categories.length) await store.dispatch("getCategories");
+};
+
+export default getContentful;
