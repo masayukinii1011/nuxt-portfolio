6fefc5d6104bf689ed4b18bc6afb49dde76392e5
masayukinii1011 <masayukinii1011@gmail.com>
2021-01-03 17:00:15 +0900

    oldページ作成

---
+++ pages/works/old.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <PostList :title="'old'.toUpperCase()" :posts="works" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from "@nuxtjs/composition-api";
+import PostList from "~/components/PostList.vue";
+
+export default defineComponent({
+  components: {
+    PostList
+  },
+  setup(_props, { root }) {
+    const works = computed(() => root.$accessor.works);
+    return {
+      works
+    };
+  }
+});
+</script>
